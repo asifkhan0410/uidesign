b19f9b9b58ee8bd264826b39a52e0445f57885f9
Asif <asifkhan0410@gmail.com>
2021-03-09 04:12:13 +0530

    5 seaction complete

--- project1/script.js
+++ project1/script.js
@@ -1,13 +1,22 @@
 const navbarList = document.querySelectorAll('.navigation__listitem');
-const navbarListBody = document.querySelectorAll('.body')
-let activepanel,activebody,currentactivebody;
+const navbarListBody = document.querySelectorAll('.body');
+const Body = document.querySelector('.navigation__body');
+const fitlerActive = document.querySelectorAll('.filteractive');
+let activepanel,activebody,currentactivebody,filteractive;
       
     function toggleTab(){
         activepanel= document.querySelector('.navigation__listitem.active');
         activebody= document.querySelector('.body.activebody');
         currentactivebody = document.querySelector(`.body.${this.classList[1]}`);
         currentactivebody.classList.add('activebody');
-        activebody.classList.remove('activebody')
+        activebody.classList.remove('activebody');
+        if(this.classList[1]==='item4'||this.classList[1]==='item5'){
+            Body.style.backgroundColor = 'white';
+            Body.style.border='none'
+        }else{
+            Body.style.background = 'rgb(239 235 235 / 30%) 0% 0% no-repeat padding-box';
+            Body.style.border='2px dashed rgb(1 35 69 / 30%)'
+        }
         
           if(!activepanel && !activebody){
             this.classList.add('active');
@@ -23,4 +32,21 @@ let activepanel,activebody,currentactivebody;
           }
       }
 
+      function toggleClick(){
+          filteractive = document.querySelector('.filteractive.clicked');
+          if(!filteractive){
+              this.style.backgroundColor='blue';
+              this.classList.add('clicked');
+          }else if(filteractive===this){
+            this.style.backgroundColor='blue';
+            this.classList.add('clicked');
+          }else{
+            this.style.backgroundColor='blue';
+            this.classList.add('clicked');
+            filteractive.style.backgroundColor='#F9F5F5';
+            filteractive.classList.remove('clicked');
+          }
+      }
+
 navbarList.forEach(nl => nl.addEventListener('click', toggleTab));
+fitlerActive.forEach(fa => fa.addEventListener('click', toggleClick));
