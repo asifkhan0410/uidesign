866881c148409df49f8a84a246f90d63fadc1541
Asif <asifkhan0410@gmail.com>
2021-03-09 03:02:40 +0530

    three section done

--- project1/script.js
+++ project1/script.js
@@ -1,15 +1,25 @@
 const navbarList = document.querySelectorAll('.navigation__listitem');
-let activepanel;
+const navbarListBody = document.querySelectorAll('.body')
+let activepanel,activebody,currentactivebody;
       
     function toggleTab(){
         activepanel= document.querySelector('.navigation__listitem.active');
-          if(!activepanel){
-              this.classList.add('active');
-          }else if(activepanel===this){
+        activebody= document.querySelector('.body.activebody');
+        currentactivebody = document.querySelector(`.body.${this.classList[1]}`);
+        currentactivebody.classList.add('activebody');
+        activebody.classList.remove('activebody')
+        
+          if(!activepanel && !activebody){
+            this.classList.add('active');
+            //currentactivebody.classList.add('activebody');
+          }else if(activepanel===this && activebody===currentactivebody){
             this.classList.add("active");
+            //currentactivebody.classList.add('activebody');
           }else{
             this.classList.add("active");
-            activepanel.classList.remove('active') 
+            activepanel.classList.remove('active') ;
+            //activepanel.classList.remove('activebody')
+            //currentactivebody.classList.remove('activebody');
           }
       }
 
